d541abd3107de106df83de4840c4e537ace7bed5
Scott Fernandez <sfernandez131@gmail.com>
2026-02-12 19:22:20 -0500

    next section

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -32,6 +34,10 @@ impl Inventory {
 }
 
 fn main() {
+    ex6();
+}
+
+fn ex1() {
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
     };
@@ -40,8 +46,82 @@ fn main() {
     let user2 = None;
 
     let giveaway = store.giveaway(user1);
-    println!("User1 got: {giveaway:?}");
+    println!("User1 with preference {user1:?} got: {giveaway:?}");
 
     let giveaway = store.giveaway(user2);
-    println!("User2 got: {giveaway:?}");
+    println!("User2 with preference {user2:?} got: {giveaway:?}");
+}
+
+fn ex2() {
+    let example_closure = || "Hello from a closure!";
+
+    println!("{0}", example_closure());
+
+    let list = vec![1, 2, 3];
+
+    println!("Before defining closure: {list:?}");
+
+    let only_borrow = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrow();
+    println!("After calling closure: {list:?}");
+}
+
+fn ex3() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || list.push(4);
+    //println!("After defining closure: {list:?}");
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+}
+
+fn ex4() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
+}
+
+fn ex5() {
+    let mut unsorted_list = vec![2, 4, 0, 1, 2, 8];
+    unsorted_list.sort();
+    unsorted_list.reverse();
+    println!("{unsorted_list:?}");
+}
+
+fn ex6() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    let mut list = [
+        Rectangle {
+            width: 3,
+            height: 2,
+        },
+        Rectangle {
+            width: 8,
+            height: 3,
+        },
+        Rectangle {
+            width: 40,
+            height: 10,
+        },
+    ];
+
+    let value = String::from("closure called");
+    let mut num_sort_ops = 0;
+    list.sort_by_key(|r| {
+        num_sort_ops += 1;
+        r.width
+    });
+
+    println!("Closure was called {num_sort_ops} times.");
 }
